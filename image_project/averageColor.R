3be8854243b90e0a1a784e3f5632f02c86c7479f
chenguanhua <ghchen5d2012@gmail.com>
2017-05-17 11:57:07 -0400

    Calculate average color of a given picture
    This script uses "Imager" package to calculate the average RGB color of
    a given picture. It could generate a preview of the picture to be
    calculated and then draw a 20*20 shade of the average color.

---
+++ image_project/averageColor.R
@@ -0,0 +1,18 @@
+#install.packages('imager')
+library('imager')
+
+#load the picture
+im <- load.image('C:\\Research\\ccs-data-scholars\\image_project\\6.jpg')
+
+#preview the picture loaded
+plot(im)
+
+#calculate the average values of the RGB channels
+averageRGB=imsplit(im,"c") %>% laply(mean)*255
+averageRGB
+
+#draw a shade with the average RGB
+imfill(20,20,val=c(averageRGB)) %>% plot
+
+
+
