9234f8257120a1a4a4334834cbb35d70b488d5fe
chenguanhua <ghchen5d2012@gmail.com>
2017-05-22 14:25:28 -0400

    fix a bug in drawing average color

--- image_project/averageColor.R
+++ image_project/averageColor.R
@@ -2,7 +2,7 @@
 library('imager')
 
 #load the picture
-im <- load.image('C:\\Research\\ccs-data-scholars\\image_project\\6.jpg')
+im <- load.image('C:\\Research\\ccs-data-scholars\\image_project\\1.jpg')
 
 #preview the picture loaded
 plot(im)
@@ -12,7 +12,5 @@ averageRGB=imsplit(im,"c") %>% laply(mean)*255
 averageRGB
 
 #draw a shade with the average RGB
-imfill(20,20,val=c(averageRGB)) %>% plot
-
-
+imfill(20,20,val=c(averageRGB,0,255)) %>% plot
 
